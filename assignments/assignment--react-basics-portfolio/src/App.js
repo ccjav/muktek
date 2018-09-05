4fedd44ed65d0278ee6d7eefcb177f9db174b919
Javier <shiordia@gmail.com>
2018-09-05 08:54:18 -0500

    Test portfolio fixed

--- assignments/assignment--react-basics-portfolio/src/App.js
+++ assignments/assignment--react-basics-portfolio/src/App.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class PortfolioFixed extends Component {
+  render() {
+    return (
+      <h1>Hola desde PortfolioFixed</h1>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="app-container">
+        <PortfolioFixed />
       </div>
     );
   }
